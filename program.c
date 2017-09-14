9f05e97389836c1f77a715a24d54698776fa286e
ChandralekhaG <31921725+ChandralekhaG@users.noreply.github.com>
2017-09-14 16:18:25 +0530

    Add files via upload

---
+++ program.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+#include<conio.h>
+
+ 
+void main()
+ 
+{
+  
+int i,arr[30],j,no;
+ 
+clrscr();
+  
+printf("Enter size of array: ");
+ 
+scanf("%d",&no);
+
+if(no<=30)
+
+{
+printf("Enter any %d elements in array: ",no);
+ 
+for(i=0;i<no;i++)
+ 
+{
+   
+scanf("%d",&arr[i]);
+  
+}
+  
+printf("Duplicate elements are: ");
+ 
+ for(i=0; i<no; i++)
+   
+{
+   
+for(j=i+1;j<no;j++)
+   
+ {
+   
+if(arr[i]==arr[j])
+  
+  {
+   
+ printf("%d\n",arr[i]);
+ 
+  }
+  
+ }
+  
+}
+}
+else
+
+printf("size exceeds");
+
+  getch();
+ 
+}
+
+
